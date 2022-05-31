00db8fda9bd97f17caf47fa84069170028893b2d
Aman Singh <reachout.amansingh@gmail.com>
2022-06-01 02:26:58 +0530

    feat : get playlist api call in context

--- src/contexts/playlist-context.js
+++ src/contexts/playlist-context.js
@@ -1,5 +1,8 @@
-import { createContext, useContext, useReducer } from "react";
+import axios from "axios";
+import { createContext, useContext, useReducer, useEffect } from "react";
 import { playlistReducer } from "../reducers";
+import { useAuth } from "../contexts";
+import { constants } from "../utils";
 
 const PlaylistContext = createContext(null);
 
@@ -12,6 +15,30 @@ const PlaylistProvider = ({ children }) => {
     playlistReducer,
     initialPlaylistState
   );
+
+  const {
+    userData: { token },
+  } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data, status } = await axios({
+          method: "GET",
+          url: "/api/user/playlists",
+          headers: { authorization: token },
+        });
+        if (status === 200) {
+          playlistDispatch({
+            type: constants.INITIALISE_PLAYLISTS,
+            payload: { initialise_playlists: data.playlists },
+          });
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [token]);
 
   return (
     <PlaylistContext.Provider value={{ playlistState, playlistDispatch }}>
--- src/reducers/playlists-reducer.js
+++ src/reducers/playlists-reducer.js
@@ -3,7 +3,7 @@ import { constants } from "../utils";
 export const playlistReducer = (state, { type, payload }) => {
   switch (type) {
     case constants.INITIALISE_PLAYLISTS:
-      return { ...state };
+      return { ...state, playlists: payload.initialise_playlists };
 
     default:
       return state;
