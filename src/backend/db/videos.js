514bc847738f4bc32a9b88caa8c80593a32cda10
Aman Singh <reachout.amansingh@gmail.com>
2022-06-08 14:46:53 +0530

    fix : update id in backend

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,11 +5,10 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_B0bI",
+    _id: "U0bhE67HuDY",
     title:
       "15 Minute Beginner Weight Training - Easy Exercises - HASfit Beginners Workout Routine - Strength",
     categoryName: "weightlifting",
-    link: "https://www.youtube.com/watch?v=U0bhE67HuDY",
     thumbnail: "https://i3.ytimg.com/vi/U0bhE67HuDY/maxresdefault.jpg",
     description:
       "Everyone has to start somewhere and this 15 minute beginner weight training routine is perfect if you're just getting started! Let Coach Kozak motivate and inspire you through this easy exercise beginner workout routine.",
@@ -18,10 +17,9 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLS8yUsoZP_JJAAdCc7AFQXutrv1tcblzl6JCS5yIQ=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "SO5rMEP_B1bI",
+    _id: "ixkQaZXVQjs",
     title: "The PERFECT Beginner Workout (Sets and Reps Included)",
     categoryName: "weightlifting",
-    link: "https://www.youtube.com/watch?v=ixkQaZXVQjs",
     thumbnail: "https://i3.ytimg.com/vi/ixkQaZXVQjs/maxresdefault.jpg",
     description:
       "If you are new to training and looking for a beginner workout to build muscle then this is the step by step workout plan you need. In this video, I take you through a workout for beginners that is 3 months in length, or possibly even longer if you decide to stick with the third month longer, that is designed to build both strength and muscle while making sure to build your body on a solid foundation.",
@@ -30,10 +28,9 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLQg6gDZ9Z0wuMSRwtdl9e0SiI0mklMDFXeGjBktXg=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "LT5dmEP_B2bI",
+    _id: "mynQqj43qls",
     title: "10 Things Nobody Tells You About Lifting Weights",
     categoryName: "weightlifting",
-    link: "https://www.youtube.com/watch?v=mynQqj43qls",
     thumbnail: "https://i3.ytimg.com/vi/mynQqj43qls/maxresdefault.jpg",
     description:
       "Can you really lift Heavier Weights at Night than in the Morning? Are some people non-responders to exercise? These are 10 things nobody tells you about lifting weights and working out and I promise they will blow your mind. Break the chains of age-old fitness myths and muscle-building lies that you still believe in. If you want to build muscle faster and avoid wasting time make sure you know about these 10 muscle growth mistakes.",
@@ -42,10 +39,9 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLSQugCJYGSrjqN0sa9VjnxQ_TIglU8QAVkiPuE=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "Go5nUEP_B3bI",
+    _id: "s2NQhpFGIOg",
     title: "15 Min Daily Yoga Routine for Beginners (Follow Along)",
     categoryName: "yoga",
-    link: "https://www.youtube.com/watch?v=s2NQhpFGIOg",
     thumbnail: "https://i3.ytimg.com/vi/s2NQhpFGIOg/maxresdefault.jpg",
     description:
       "This is a 15 minute beginner’s daily yoga routine. With gyms and offices shutting down, this video is focussed on utilising our precious time into a higher purpose of learning yoga. If you have never practised any of the yogic postures or breathing exercises then this should be the video which will help you get started with this divine practice. Let’s not focus on the fear, panic and the noise created by the media instead channelise our energy into something like yoga which will boost your immunity and help you live a disease free life in the long run. One more thing, this is not a video to just watch but to follow along. So, c’mon let us do it together.",
@@ -54,10 +50,9 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLSEV5H0Dc_SHuQwWmvhnSJG4guCrWjvhTe8wlzLKA=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "Po5XMrP_B4fI",
+    _id: "hJbRpHZr_d0",
     title: "Yoga For Anxiety and Stress",
     categoryName: "yoga",
-    link: "https://www.youtube.com/watch?v=hJbRpHZr_d0",
     thumbnail: "https://i3.ytimg.com/vi/hJbRpHZr_d0/maxresdefault.jpg",
     description:
       "Yoga For Anxiety and Stress. Awaken The Force! Move from the darkness into the light! Yoga has your back! In this practice video Adriene guides you through breath and body practices that serve you when you feel bad. Learn breathing techniques and spinal work that will assist you in moments of stress. Find release, create space and process yo' stuff. Bookmark this video. Yoga to the rescue! Enjoy.",
@@ -66,10 +61,9 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLSsqgadKGeiBj_sxb1dC5_msmo_94JIIr4EPUdcFQ=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "Mo5dMlP_t5bI",
+    _id: "X655B4ISakg",
     title: "Yoga for Kids!",
     categoryName: "yoga",
-    link: "https://www.youtube.com/watch?v=X655B4ISakg",
     thumbnail: "https://i3.ytimg.com/vi/X655B4ISakg/maxresdefault.jpg",
     description:
       "Follow along as Sophia Khan leads a fun and family-friendly introduction to yoga. You'll get to warm up, practice breathing and poses, and relax into a power down.",
@@ -78,10 +72,9 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLQ4cu6Qig0rtxEhdStQOT01RB4NdZOQNfHmcxK6pQ=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "Ro2ddEP_B6bI",
+    _id: "X6395gykhg",
     title: "WEIGHT LOSS - Indian Weight Loss Diet",
     categoryName: "diet",
-    link: "https://www.youtube.com/watch?v=-X6395gykhg",
     thumbnail: "https://i3.ytimg.com/vi/-X6395gykhg/maxresdefault.jpg",
     description:
       "Weight loss, Indian weight loss diet plan for men and women. This is a budget friendly, practical, vegetarian and non vegetarian diet plan for weight loss. This indian meal plan is ideal for weight loss (fat loss). This Indian weight loss diet may help you shed more than 10 kg. This Indian diet chart has many options to make it a weekly diet chart for weight loss (fat loss). How to lose weight fast? Weight loss tips? Diet for weight loss? Diet for fat loss? How to get rid of belly fat? Indian meal prep for the week to lose weight for women and men? Now these questions won't worry you anymore.",
@@ -90,10 +83,9 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLSEV5H0Dc_SHuQwWmvhnSJG4guCrWjvhTe8wlzLKA=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "Jo5ndeP_s7bI",
+    _id: "Gmh_xMMJ2Pw",
     title: "How to Create a Healthy Plate",
     categoryName: "diet",
-    link: "https://www.youtube.com/watch?v=Gmh_xMMJ2Pw",
     thumbnail: "https://i3.ytimg.com/vi/Gmh_xMMJ2Pw/maxresdefault.jpg",
     description:
       "A healthy plate is an easy way to control portion sizes. Fill half your plate with nonstarchy vegetables, one quarter with lean protein, and one with whole grains for a healthy eating plan.",
@@ -102,10 +94,9 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLTgCGOsCCoY689hswdv24tcVGZpQTh9zkmUwf5Dww=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "qo5dChP_g8bI",
+    _id: "6ZUFT0CwU1w",
     title: "The Only Diet Plan That Ayurveda Recommends (Men & Women)",
     categoryName: "diet",
-    link: "https://www.youtube.com/watch?v=6ZUFT0CwU1w",
     thumbnail: "https://i3.ytimg.com/vi/6ZUFT0CwU1w/maxresdefault.jpg",
     description:
       "The only diet plan that Ayurveda recommends in it's scriptures",
@@ -114,10 +105,9 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLSEV5H0Dc_SHuQwWmvhnSJG4guCrWjvhTe8wlzLKA=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "Io5dwEP_i9bI",
+    _id: "gCROJglfRxo",
     title: "7 DAY CHALLENGE: Lose Belly & Arms Fat | Zumba Class",
     categoryName: "zumba",
-    link: "https://www.youtube.com/watch?v=gCROJglfRxo",
     thumbnail: "https://i3.ytimg.com/vi/gCROJglfRxo/maxresdefault.jpg",
     description: "7 DAY CHALLENGE: Lose Belly & Arms Fat | Zumba Class",
     creator: "Zumba Class",
@@ -125,10 +115,9 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLTknUZOFNUGe0tCw9v_G26UqzqVx5nxTqrOq4bQ=s48-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "vo5dgEP_o10bI",
+    _id: "OGCX_WU6vRc",
     title: "Kamelia - Amor (Zumba Choreography by Claudiu Gutu)",
     categoryName: "zumba",
-    link: "https://www.youtube.com/watch?v=OGCX_WU6vRc",
     thumbnail: "https://i3.ytimg.com/vi/OGCX_WU6vRc/maxresdefault.jpg",
     description:
       "Have fun while you lose weight! Zumba Dance Workout with our best uDance instructor! Keep on doing this dance non stop to lose weight while dancing and having fun.  Zumba is an enjoyable way to stay fit.",
@@ -137,11 +126,10 @@ export const videos = [
       "https://yt3.ggpht.com/ytc/AKedOLRWqrKoyhwhQDsn1SSRNJS37HBn1WlSS17Q4ww4=s48-c-k-c0x00ffffff-no-rj-mo",
   },
   {
-    _id: "Mo5dKfP_e11bI",
+    _id: "l3ullc3u7HE",
     title:
       "Bollywood Dance Cardio 15 Minute NonStop Workout | High and Low Impact | Rangeela Dance Company",
     categoryName: "zumba",
-    link: "https://www.youtube.com/watch?v=l3ullc3u7HE",
     thumbnail: "https://i3.ytimg.com/vi/l3ullc3u7HE/maxresdefault.jpg",
     description:
       "Grab that water bottle and towel to wipe off the sweat, you'll need it for this one! Burn calories fast with our 15-minute heart-pumping Bollywood cardio dance workout you can do from home. Our instructors will guide you through a warmup, followed by high energy Bollywood cardio steps and finish with a cool-down leaving you wanting more",
