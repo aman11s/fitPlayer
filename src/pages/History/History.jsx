bff5a214e2f0e5f5775573f491d4bd34e344cc9f
Aman Singh <reachout.amansingh@gmail.com>
2022-06-10 17:48:59 +0530

    feat : add page loader and header

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { VideoCard } from "../../components";
+import { ClipLoader } from "react-spinners";
+import { PageHeader, VideoCard } from "../../components";
 import { useAuth } from "../../contexts";
 
 export const History = () => {
@@ -9,10 +10,12 @@ export const History = () => {
   } = useAuth();
 
   const [historyVideos, setHistoryVideos] = useState([]);
+  const [pageLoader, setPageLoader] = useState(false);
 
   useEffect(() => {
     (async () => {
       try {
+        setPageLoader(true);
         const { data, status } = await axios({
           method: "GET",
           url: "/api/user/history",
@@ -23,13 +26,32 @@ export const History = () => {
         }
       } catch (e) {
         console.error(e);
+      } finally {
+        setPageLoader(false);
       }
     })();
   }, [token]);
 
+  const pageHeaderDetails = {
+    pageTitle: "History",
+    subTitle: "video",
+    state: historyVideos,
+  };
+
+  if (pageLoader) {
+    return (
+      <>
+        <main className="main-container container-flex-center main-min-height">
+          <ClipLoader color="#ef6236" speedMultiplier={2} size={40} />
+        </main>
+      </>
+    );
+  }
+
   return (
     <>
       <main className="main-container main-min-height">
+        <PageHeader pageHeaderDetails={pageHeaderDetails} />
         <div className="grid-minmax-card m-4">
           {historyVideos.map((videos) => {
             const historyProps = {
